618d42d29cdb004012f5d87e870b0f3e827ed715
Moet.FE.Paco <paco@w07.tech>
2026-06-24 14:21:28 +0700

    Fix Swagger UI assets on Vercel

--- src/configure-app.ts
+++ src/configure-app.ts
@@ -1,4 +1,4 @@
-import { INestApplication, ValidationPipe } from "@nestjs/common";
+import { INestApplication, RequestMethod, ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { PrismaExceptionFilter } from "./common/filters/prisma-exception.filter";
@@ -6,7 +6,12 @@ import { PrismaExceptionFilter } from "./common/filters/prisma-exception.filter"
 export function configureApp(app: INestApplication) {
   const config = app.get(ConfigService);
 
-  app.setGlobalPrefix("api");
+  app.setGlobalPrefix("api", {
+    exclude: [
+      { path: "docs", method: RequestMethod.GET },
+      { path: "docs-json", method: RequestMethod.GET },
+    ],
+  });
   app.enableCors({
     origin: config.get<string>("CORS_ORIGIN")?.split(",") ?? ["http://localhost:3000"],
     credentials: true,
@@ -27,5 +32,41 @@ export function configureApp(app: INestApplication) {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup("docs", app, document);
+  const httpAdapter = app.getHttpAdapter();
+
+  httpAdapter.get("/docs-json", (_request, response) => {
+    response.json(document);
+  });
+
+  httpAdapter.get("/docs", (_request, response) => {
+    response.type("html").send(renderSwaggerHtml());
+  });
+}
+
+function renderSwaggerHtml() {
+  return `<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Sales Debt Management API</title>
+    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.17.14/swagger-ui.css" />
+  </head>
+  <body>
+    <div id="swagger-ui"></div>
+    <script src="https://unpkg.com/swagger-ui-dist@5.17.14/swagger-ui-bundle.js"></script>
+    <script src="https://unpkg.com/swagger-ui-dist@5.17.14/swagger-ui-standalone-preset.js"></script>
+    <script>
+      window.onload = () => {
+        window.ui = SwaggerUIBundle({
+          url: "/docs-json",
+          dom_id: "#swagger-ui",
+          deepLinking: true,
+          presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+          layout: "StandaloneLayout"
+        });
+      };
+    </script>
+  </body>
+</html>`;
 }
